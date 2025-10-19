eac05de725118cfd4798df84981b4e2e2ac6bee7
poomthep <poomthep@gmail.com>
2025-10-19 21:10:53 +0700

    Update config and force redeploy

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 // js/config.js
-// ไฟล์นี้จะพยายามอ่านค่าจาก window object
-// ซึ่งจะถูก "ฉีด" เข้ามาโดยไฟล์อื่น (สำหรับ local) หรือโดย Hosting Provider (สำหรับ production)
-export const SUPABASE_URL = window.SUPABASE_URL;
-export const SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY;
+
+// ใส่ Supabase URL และ Public Anon Key ของคุณตรงนี้
+// คีย์นี้ปลอดภัยที่จะเปิดเผยในโค้ดฝั่งเบราว์เซอร์
+export const SUPABASE_URL = "https://sihvgfnvleoloyhzcgll.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpaHZnZm52bGVvbG95aHpjZ2xsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3NTEzNzAsImV4cCI6MjA3NjMyNzM3MH0.QVGsCHFmmzytiwtF90KNCIsHjCw4r15omI9RMTmCFxw"; // << ใส้คีย์ ANON KEY เต็มๆ ของคุณที่นี่
